f9b79d7ccaf3e595ad3efd1e5e6f8c552d64f2eb
Richard Helgeby <richard.helgeby@outlook.com>
2023-07-29 20:30:43 +0200

    Add some comments about the exit command.

--- src/main/java/no/helgeby/hangman/command/ExitCommand.java
+++ src/main/java/no/helgeby/hangman/command/ExitCommand.java
@@ -15,7 +15,11 @@ public class ExitCommand implements CommandHandler {
 
 	@Override
 	public CommandResult handleCommand(String commandLine, String baseCommand, StringTokenizer tokens) {
+		// This is OK for stand-alone apps. If it's in a container we don't want to shut
+		// down like this.
 		System.exit(0);
+
+		// No effect, but must be here to make compiler happy.
 		return SUCCESS;
 	}
 
